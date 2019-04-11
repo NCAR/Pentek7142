ea6f37ba1f43ff6628f8b0ee56f333bbc7270142
Gary Granger <granger@ucar.edu>
2019-04-11 22:59:02 +0000

    specify DDC type on command line
    Keep the p7142sd3c constructor from bailing early by specifying the
    required DDC type name as a command-line argument.

--- test/sd3c_info_7142.cpp
+++ test/sd3c_info_7142.cpp
@@ -1,4 +1,4 @@
-/*
+/* -*- mode: C++; indent-tabs-mode: nil; c-basic-offset: 4; -*-
  * sd3c_info_7142.cpp
  *
  * Print information about the SD3C bistream (if any) installed on a Pentek
@@ -36,14 +36,38 @@
 
 LOGGING("sd3c_info_7142")
 
+using namespace Pentek;
+
 int
 main(int argc, char** argv)
 {
-	// Let logx get and strip out its arguments
-	logx::ParseLogArgs(argc, argv);
+    // Let logx get and strip out its arguments
+    logx::ParseLogArgs(argc, argv);
 
-	// Instantiate a p7142sd3c object using the first Pentek 7142 on the system.
-	// The constructor will log the SD3C DDC type and revision number.
-	Pentek::p7142sd3c dev(false, 0.0, 0.0, 0.001, 0.0, false, 100, 1, false,
-			Pentek::p7142sd3c::DDCUNDEFINED, false, 0.0, true, false, 0, 0);
+    p7142sd3c::DDCDECIMATETYPE ddctype = Pentek::p7142sd3c::DDCUNDEFINED;
+
+    std::string ddcname;
+    if (argc > 1)
+	ddcname = argv[1];
+
+    p7142sd3c::DDCDECIMATETYPE knowntypes[] = {
+	p7142sd3c::DDCUNDEFINED, p7142sd3c::DDC10DECIMATE, 
+        p7142sd3c::DDC8DECIMATE, p7142sd3c::DDC6DECIMATE, 
+        p7142sd3c::DDC4DECIMATE, p7142sd3c::BURST
+    };
+
+    for (int i=0; i < sizeof(knowntypes)/sizeof(knowntypes[0]); ++i)
+    {
+        if (p7142sd3c::ddcTypeName(knowntypes[i]) == ddcname)
+        {
+            ddctype = knowntypes[i];
+            break;
+        }
+    }
+
+    // Instantiate a p7142sd3c object using the first Pentek 7142 on the system.
+    // The constructor will log the SD3C DDC type and revision number.
+    Pentek::p7142sd3c dev(false, 0.0, 0.0, 0.001, 0.0, false, 100, 1, false,
+			  ddctype, false, 0.0, true, false, 0, 0);
 }
+
